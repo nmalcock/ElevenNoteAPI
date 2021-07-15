0cda53d45ddbc0f1e46117cdc86a1dc033aacd0d
nmalcock <nmalcock97@gmail.com>
2021-07-15 12:29:16 -0400

    Added Note Controller

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,22 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+    }
+}
